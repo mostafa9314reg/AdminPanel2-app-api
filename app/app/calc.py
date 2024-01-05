91277797116fc74379ecf3230cd1b24715e2e2b6
mosttafa9314reg <mostafa9314reg@gmail.com>
2024-01-05 13:13:07 +0330

    installing postgres

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+This is test function to testify unit test by django
+"""
+
+def func_add(x,y):
+
+    return x+y
+
+def func_subtract(x,y):
+
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+This is test module for unit test in django
+
+"""
+
+
+from app import calc
+from django.test import SimpleTestCase
+
+class TestAddFunc(SimpleTestCase):
+
+    def tester_adding(self):
+        
+        res = calc.func_add(4,5)
+        self.assertEqual(res,9)
+
+
+    def tester_subtracting(self):
+        res = calc.func_subtract(5,5)
+        self.assertEqual(res,0)
