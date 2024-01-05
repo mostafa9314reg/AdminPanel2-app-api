6726fdf285d5afd270bcf82e662a5852c136ab54
mosttafa9314reg <mostafa9314reg@gmail.com>
2024-01-05 14:17:39 +0330

    fixing lint error

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,13 @@
 This is test function to testify unit test by django
 """
 
-def func_add(x,y):
+
+def func_add( x , y ):
 
     return x+y
 
-def func_subtract(x,y):
+
+def func_subtract( x , y ):
 
     return x-y
 
