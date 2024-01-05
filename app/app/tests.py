0291523426b68cd01002c9c6f5feaafcd3ba8ac8
mosttafa9314reg <mostafa9314reg@gmail.com>
2024-01-05 13:49:40 +0330

    fixing lint error

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,7 +3,6 @@ This is test module for unit test in django
 
 """
 
-
 from app import calc
 from django.test import SimpleTestCase
 
@@ -16,6 +15,6 @@ class TestAddFunc(SimpleTestCase):
 
 
     def tester_subtracting(self):
-        res = calc.func_subtract(5,5)
-        self.assertEqual(res,0)
 
+        res = calc.func_subtract(5, 5)
+        self.assertEqual(res, 0)
