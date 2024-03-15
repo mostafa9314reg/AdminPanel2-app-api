11136110c08e7b4ae8e37150798338edf839bc3a
mosttafa9314reg <mostafa9314reg@gmail.com>
2024-03-15 18:54:54 +0330

    writing command wait_for_db  method for tdd race condition

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,4 +11,15 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) :    
         # return super().handle(*args, **options)
-        pass
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
