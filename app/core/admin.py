1d85494aa664d4dbe5c420bf0d6a39255bea4c44
mosttafa9314reg <mostafa9314reg@gmail.com>
2024-04-22 15:37:14 +0330

    writing useradmin for test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
+"""
+Django admin customization.
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+
+
+class MyUserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email','name']
+
+
+admin.site.register(models.User,MyUserAdmin)
